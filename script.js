0dc3e4311aebdc31ae5ec2c1f441231eda475196
simblitz@gmail.com <simblitz@gmail.com>
2022-03-30 14:01:56 -0400

    refactor media query for responsiveness

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ COPY_RIGHT_YEAR.innerHTML = new Date().getFullYear();
 window.onscroll = () => {
     let currentScrollPosition = window.pageYOffset;
     if (prevScrollPosition > currentScrollPosition) {
-        document.getElementById("header").style.top = "0";
+        document.getElementById("").style.top = "0";
     } else {
         document.getElementById("header").style.top = "-98px";
     }
